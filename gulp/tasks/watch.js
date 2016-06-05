ac93e7f68dc53c3e8c170a7237d674bb22d7cbf1
knt5 <knt5@users.noreply.github.com>
2016-06-05 23:26:09 +0900

    Add gulp tasks

---
+++ gulp/tasks/watch.js
@@ -0,0 +1,20 @@
+var gulp = require('gulp');
+//var config = require('../config/config');
+
+gulp.task('watch', function() {
+	
+	
+	
+	/*
+	// Build targets
+	gulp.watch(config.build.watchTargets, ['build']);
+	
+	// Lint targets: scss and js
+	gulp.watch(config.lint.targets.scss, ['lint:scss']);
+	gulp.watch(config.lint.targets.js, ['lint:js']);
+	
+	// Lint targets: built html and js
+	gulp.watch(config.lint.targets.built.html, ['lint:html:built']);
+	gulp.watch(config.lint.targets.built.js, ['lint:js:built']);
+	*/
+});
