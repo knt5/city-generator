a67216c83d3e857610d472ae35825ebeb62f4fba
knt5 <knt5@users.noreply.github.com>
2016-06-07 00:49:31 +0900

    Add build:css task

---
+++ gulp/modules/build-css/config.js
@@ -0,0 +1,4 @@
+module.exports = {
+	src: 'src/scss/**/*.scss',
+	dest: 'gulp/works/css/'
+};
--- gulp/modules/build-css/index.js
+++ gulp/modules/build-css/index.js
@@ -1,4 +1,16 @@
 const gulp = require('gulp');
+const rename = require('gulp-rename');
+const sass = require('gulp-ruby-sass');
+const config = require('./config');
 
 module.exports = () => {
+	return sass(config.src, {
+		style: 'compressed'
+	})
+	.on('error', sass.logError)
+	.pipe(rename({
+		dirname: '',
+		extname: '.mustache'
+	}))
+	.pipe(gulp.dest(config.dest));
 };
