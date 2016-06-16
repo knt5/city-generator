28c57456e25c139ada1753171a99d5f0f58cad23
knt5 <knt5@users.noreply.github.com>
2016-06-16 22:22:20 +0900

    Add a comment of getPixelRatio

--- src/js/index/utils/deviceUtil.js
+++ src/js/index/utils/deviceUtil.js
@@ -1,6 +1,8 @@
 export default {
 	getPixelRatio() {
 		if (window.devicePixelRatio !== undefined) {
+			// TODO: Detect whether the device is poor or not,
+			//       and return devicePixelRatio to draw.
 			return window.devicePixelRatio;
 		} else {
 			return 1;
